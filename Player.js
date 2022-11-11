7f431876f5f048f0cf451b2c4d9b3b42d3ba5c7a
Scott Atkinson <scott.atkin79@gmail.com>
2022-11-11 19:18:18 +0000

    add prevent default to touch controls

--- Player.js
+++ Player.js
@@ -24,6 +24,7 @@ export default class Player {
 
         window.addEventListener("touchstart", e => {
             this.touchX = e.changedTouches[0].pageX;
+            e.preventDefault;
         });
         window.addEventListener("touchmove", e => {
             if (this.touchX < this.canvas.width / 2) {
